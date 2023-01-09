0e925bd4ebdcec9b136b738789956e52352cccc0
Yoon_Koh <bear04012@gmail.com>
2023-01-09 13:25:44 +0900

    Use data and methods for the first time

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container">
-    <a href="/users">Users</a>
+    <nuxt-link to="/users">Users</nuxt-link>
   </section>
 </template>
 
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>This is the users page</h1>
+  <div>
+    <input type="text" v-model="userId"/>
+    <button @click="onLoadUser">Load User</button>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: '',
+    }
+  },
+
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/' + this.userId);
+    }
+  }
+}
+</script>
