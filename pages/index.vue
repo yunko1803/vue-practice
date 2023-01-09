b1562deaddb5ab2af8944fa555849915b42e2f6b
Yoon_Koh <bear04012@gmail.com>
2023-01-09 13:07:52 +0900

    Set up

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container">
+    <a href="/users">Users</a>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
