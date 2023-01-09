f3e6313e176e3d208d6b3aca04278c2a86406a09
Yoon_Koh <bear04012@gmail.com>
2023-01-09 22:14:32 +0900

    Create header component and use Emit function to interect each component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav"/>
+    <TheSidenav
+      :show="displaySidenav"
+      @close="displaySidenav = false"
+    />
     <Nuxt />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import TheHeader from '@/components/Navigation/TheHeader.vue'
+import TheSidenav from '@/components/Navigation/TheSidenav.vue'
 
 export default Vue.extend({
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySidenav: false
+    }
+  }
 })
 </script>
 
